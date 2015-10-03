3f63ea3a1585f72cb40d910ea3f74502bcbf0f7e
Daniel Kawamoto <djkawamoto@gmail.com>
2015-10-03 10:35:42 -0700

    BibleTranslation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003171743) do
+ActiveRecord::Schema.define(version: 20151003172846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20151003171743) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "bible_translations", force: :cascade do |t|
+    t.string   "description"
+    t.string   "abbreviation"
+    t.string   "language"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "denominations", force: :cascade do |t|
