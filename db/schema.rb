b5b93e873e142f873d4aeb8b3295d73c1283346f
Daniel Kawamoto <djkawamoto@gmail.com>
2015-10-03 10:40:29 -0700

    Course

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003172846) do
+ActiveRecord::Schema.define(version: 20151003173834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20151003172846) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
   create_table "denominations", force: :cascade do |t|
     t.string   "description"
@@ -66,5 +76,6 @@ ActiveRecord::Schema.define(version: 20151003172846) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "courses", "users"
   add_foreign_key "user_actions", "users"
 end
