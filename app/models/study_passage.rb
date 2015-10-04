945fd0978ec364e424f26030579101a6e2f2bda1
David Bates <dlbates@kpmg.com>
2015-10-04 12:33:23 -0700

    Fixed passage annotation intersection entity.

--- app/models/study_passage.rb
+++ app/models/study_passage.rb
@@ -1,10 +1,10 @@
 class StudyPassage < ActiveRecord::Base
   belongs_to :study
   belongs_to :bible_translation
-  belongs_to :bible_passage_annotation
+  has_many :bible_passage_annotations
   belongs_to :bible_book
 
-  has_many :annotations, -> { order 'bible_passage_annotations.position' }, through: :bible_passage_annotation
+  has_many :annotations, -> { order 'bible_passage_annotations.position' }, through: :bible_passage_annotations
 
   def to_h
     {
